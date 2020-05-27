e41ed0bcba76a36fd38394b3c26fcf50e040a97d
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-05-27 15:14:02 -0500

    update placeholder with initial pokemon
    -Deleted placeholder text for file
    -introduced pokemonList array
    -added 3 starter type objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello World');
+var pokemonList = [];
 
-var favoriteFood = 'Fajitas';
-document.write(favoriteFood);
+pokemonList[0] = {
+	name: 'Bulbasaur',
+	heightMeters: 0.7,
+	type: 'grass, poison',
+	weightKg: 6.9,
+	evolvesInto: 'Ivysaur'
+};
+
+pokemonList[1] = {
+	name: 'Charmander',
+	height: 0.6,
+	type: 'fire',
+	weightKg: 8.5,
+	evolvesInto: 'Charmeleon'
+};
+
+pokemonList[2] = {
+	name: 'Squirtle',
+	height: 0.5,
+	type: 'water',
+	weightKg: 9,
+	evolvesInto: 'Wartortle'
+};
+
+console.log(pokemonList);
