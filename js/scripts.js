e52dff5fa2c89e18a8c7b314b245d09715e9927a
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-05-28 18:41:54 -0500

    add conditonal to add function
    -only allows the addition of objects in the pokemon list
    -also added two new pokemon to test code

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ var pokemonRepository = (function() {
 
 	// Create function to add new pokemon
 	function add(pokemon) {
-		pokemonList.push(pokemon);
+		if (typeof pokemon === 'object') {
+			//Allowed only objects to be added
+			return pokemonList.push(pokemon);
+		}
 	}
 
 	// Create function to list current pokemon
@@ -45,6 +48,22 @@ pokemonRepository.add({
 	evolvesInto: 'Wartortle'
 });
 
+pokemonRepository.add({
+	pokemonName: 'Pikachu',
+	heightMeters: 0.4,
+	pokemonType: [ 'electric' ],
+	weightKg: 6,
+	evolvesInto: 'Raichu'
+});
+
+pokemonRepository.add({
+	pokemonName: 'Snorlax',
+	heightMeters: 2.1,
+	pokemonType: [ 'normal' ],
+	weightKg: 460,
+	evolvesInto: null
+});
+
 //Create function then run the forEach declaration after
 function printPokemonList(pokemon) {
 	// Create a loop to list out the pokemon in order
@@ -63,19 +82,19 @@ function printPokemonList(pokemon) {
 
 pokemonRepository.getAll().forEach(printPokemonList);
 
-function printPokemonList(pokemon) {
-	// Create a loop to list out the pokemon in order
-	if (pokemon.heightMeters > 1) {
-		document.write(
-			'<p>' +
-				pokemon.pokemonName +
-				' (' +
-				pokemon.heightMeters +
-				" meters) - <span class='big-boys'>Wow, that's big!</span></p>"
-		);
-	} else {
-		document.write('<p>' + pokemon.pokemonName + ' (' + pokemon.heightMeters + ' meters)</p>');
-	}
-}
+// function printPokemonList(pokemon) {
+// 	// Create a loop to list out the pokemon in order
+// 	if (pokemon.heightMeters > 1) {
+// 		document.write(
+// 			'<p>' +
+// 				pokemon.pokemonName +
+// 				' (' +
+// 				pokemon.heightMeters +
+// 				" meters) - <span class='big-boys'>Wow, that's big!</span></p>"
+// 		);
+// 	} else {
+// 		document.write('<p>' + pokemon.pokemonName + ' (' + pokemon.heightMeters + ' meters)</p>');
+// 	}
+// }
 
-pokemonRepository.getAll().forEach(printPokemonList);
+// pokemonRepository.getAll().forEach(printPokemonList);
