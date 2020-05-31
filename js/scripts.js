5376a89b148bc5890d4cfece820e2aaf0fe828c2
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-05-31 18:02:22 -0500

    replace pomeon list with new empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // IIFE
 var pokemonRepository = (function() {
 	// Create initial empty array for Pokedex app
-	var pokemonList = [];
+	var pokemonArray = [];
 
 	// Create function to add new pokemon
 	function add(pokemon) {
 		if (typeof pokemon === 'object') {
 			//Allowed only objects to be added
-			return pokemonList.push(pokemon);
+			return pokemonArray.push(pokemon);
 		}
 	}
 
 	// Create function to list current pokemon
 	function getAll() {
-		return pokemonList;
+		return pokemonArray;
 	}
 
 	function addListItem(pokemon) {
@@ -58,51 +58,10 @@ var pokemonRepository = (function() {
 
 pokemonRepository.getAll();
 
-//Establish entries in Pokedex array
-pokemonRepository.add({
-	pokemonName: 'Bulbasaur',
-	heightMeters: 0.7,
-	pokemonType: [ 'grass', 'poison' ],
-	weightKg: 6.9,
-	evolvesInto: 'Ivysaur'
-});
-
-pokemonRepository.add({
-	pokemonName: 'Charizard',
-	heightMeters: 1.7,
-	pokemonType: [ 'fire' ],
-	weightKg: 8.5,
-	evolvesInto: 'none'
-});
-
-pokemonRepository.add({
-	pokemonName: 'Squirtle',
-	heightMeters: 0.5,
-	pokemonType: [ 'water' ],
-	weightKg: 9,
-	evolvesInto: 'Wartortle'
-});
-
-pokemonRepository.add({
-	pokemonName: 'Pikachu',
-	heightMeters: 0.4,
-	pokemonType: [ 'electric' ],
-	weightKg: 6,
-	evolvesInto: 'Raichu'
-});
-
-pokemonRepository.add({
-	pokemonName: 'Snorlax',
-	heightMeters: 2.1,
-	pokemonType: [ 'normal' ],
-	weightKg: 460,
-	evolvesInto: null
-});
-
 //Create function then run the forEach declaration after
-function printPokemonList(variable) {
+function printPokemonArray(variable) {
 	// Selects the list element in index
 	pokemonRepository.addListItem(variable);
 }
 
-pokemonRepository.getAll().forEach(printPokemonList);
+pokemonRepository.getAll().forEach(printPokemonArray);
